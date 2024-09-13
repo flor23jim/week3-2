4b51f2d57bfbc480bbf5a1b28dbdb452da267b56
Jimenez <FNJimenez@cps.k12.il.us>
2024-09-13 12:27:50 -0500

    challengeday2

---
+++ challenge.py
@@ -0,0 +1,6 @@
+name=input("what is your name?") # asking for name 
+amount=float(input("how much did you earn in commissions this month?")) # amount of commisiosn they are entitled to. 
+R=(float(amount) * .13) # multilping amount of commsion with 13% of their total sale. 
+print("hello "+ str(name)+ " you made " + str(amount)+ " in commisions and should be recieving "+ str(R) + "dollars.") # answer in one sentence 
+
+
---
+++ testColab.py
@@ -0,0 +1,11 @@
+#this is just for testing 
+#for collab purpose
+#create an input for the user to input their age
+#create an input for the user to input their  name 
+#create an input for the user to input their favorite song
+age=input( "how old are you?")
+print (" I am" +age )
+name= input("what is your name?")
+print (" my name is " + name)
+song=input("what is your favorite song")
+print (" My favorite song is "+ song)
